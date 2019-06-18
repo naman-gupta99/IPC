9c4ff69661e4c3bc4c96cc54faed8c616dce20ed
Karan <karangupta199920@gmail.com>
2019-06-18 19:58:26 +0530

    frontend: Username Validation Added

--- frontend/src/app/home/pages/signup-page/signup-page.component.ts
+++ frontend/src/app/home/pages/signup-page/signup-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-signup-page',
@@ -7,12 +9,44 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./signup-page.component.scss']
 })
 export class SignupPageComponent implements OnInit {
-
-  username = new FormControl('');
-
-  constructor() { }
+  imageUrl: any = 'http://i.pravatar.cc/500?img=7';
+  signUpForm: FormGroup;
+  forbiddenUsernames = [];
+  userNameValid = false;
+  currentUserName: string;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required, Validators.maxLength(20)], this.validateUsername.bind(this))
+    });
   }
 
+  validateUsername(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve) => {
+      setTimeout(() => {
+        // call server and query for entered username in database
+        this.forbiddenUsernames = this.authService.getUsernames();
+        this.currentUserName = control.value;
+        if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+          resolve({ 'forbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onImageUploaded(event) {
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event) => {
+      this.imageUrl = reader.result;
+    };
+  }
+  onSubmit() {
+    this.signUpForm.reset();
+    this.authService.addUsername(this.currentUserName);
+  }
 }
