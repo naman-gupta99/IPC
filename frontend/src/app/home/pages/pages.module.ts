7e2e019f22a96f6a6832cf447ba69bea5f645aa7
Karan <karangupta199920@gmail.com>
2019-09-19 23:48:32 +0530

    frontend: sign-up page added

--- frontend/src/app/home/pages/pages.module.ts
+++ frontend/src/app/home/pages/pages.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { ConnectedPageComponent } from './connected-page/connected-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ConnectedPageComponent, SignupPageComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [ConnectedPageComponent, SignupPageComponent]
 })
